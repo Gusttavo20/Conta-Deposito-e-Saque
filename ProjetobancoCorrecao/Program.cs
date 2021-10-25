4f55abab769395dff4f6e5f141a71c1ff8d5af5a
Gustavo Marques <marquesgusttavo17@gmail.com>
2021-10-25 09:51:59 -0300

    projeto criado

---
+++ ProjetobancoCorrecao/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using System.Globalization;
+
+namespace ProjetobancoCorrecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o títular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá  depósito inicial (s/n)? ");
+            char res = char.Parse(Console.ReadLine());
+            if (res == 's' || res == 'S')
+            {
+                Console.Write("Entre o valor de depósito ínicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("");
+
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("");
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+
+
+
+
+
+
+        }
+    }
+}
